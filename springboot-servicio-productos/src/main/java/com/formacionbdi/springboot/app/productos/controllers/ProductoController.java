4fe2ba7e7e2eea928fae3da02e4c3e81e76b70e2
Angel Jesus Martinez Aguirre <zoroastroo@live.com>
2020-03-01 04:03:13 -0600

    Clase 19 Probando balanceo de carga con Feign

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,10 +1,12 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +18,24 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 public class ProductoController {
 		
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private IProductoService iproductoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> Listar(){
-	return iproductoService.findAll();
+	return iproductoService.findAll().stream().map(producto -> { 
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return producto;
+	}).collect(Collectors.toList());
 	}	
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return iproductoService.findById(id);
+		Producto producto = iproductoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return producto;
 	}
 	
 }
